2c2f463cce5b6e4758de27a44a727c5bbc50e236
sperBruno <sperbru797@gmail.com>
2017-12-25 17:11:46 -0400

    removing test and adding jar files

--- src/GraphUtils.java
+++ src/GraphUtils.java
@@ -1,9 +1,6 @@
 
-
-
-
+import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
 
@@ -17,14 +14,14 @@ public class GraphUtils {
 
 	public static int minDistance(Graph graph, String src, String dest) {
 
-		if(graph == null || src == null || dest == null) {
+		if (graph == null || src == null || dest == null) {
 			return -1;
 		}
-		if(graph.containsElement(src) == false || graph.containsElement(dest)== false ){
+		if (graph.containsElement(src) == false || graph.containsElement(dest) == false) {
 			return -1;
 		}
-		if(Objects.equals(src, dest)){
-			System.out.println(src + " == " + dest);
+		if (Objects.equals(src, dest)) {
+			// System.out.println(src + " == " + dest);
 			return 0;
 		}
 
@@ -35,26 +32,37 @@ public class GraphUtils {
 		if (result == false) {
 			return -1;
 		}
-		System.out.println();
-		System.out.println(bfs.marked);
-		System.out.println(bfs.graph.getNumNodes());
-		return bfs.marked.size()> 1 ? bfs.marked.size() / 2: 1;
+		/*
+		 * System.out.println(); System.out.println(bfs.marked);
+		 * System.out.println(bfs.graph.getNumNodes());
+		 */
+		return bfs.marked.size() > 1 ? bfs.marked.size() / 2 : 1;
 	}
-	
 
 	public static Set<String> nodesWithinDistance(Graph graph, String src, int distance) {
 
-		/* IMPLEMENT THIS METHOD! */
-		
-		return null; // this line is here only so this code will compile if you don't modify it
-	}
+		if (graph == null || src == null || distance <= 0) {
+			return null;
+		}
 
+		BreadthFirstSearch bfs = new BreadthFirstSearch(graph);
+		boolean srcExistInGraph = bfs.bfs(graph.getStartingNode(), src);
+
+		if (!srcExistInGraph)
+			return null;
+
+		Set<String> nodes = new HashSet<>();
+		System.out.println("Distance " + distance);
+		System.out.println("Number of edges in graph " + graph.getNumEdges());
+
+		return nodes;
+	}
 
 	public static boolean isHamiltonianPath(Graph g, List<String> values) {
 
 		/* IMPLEMENT THIS METHOD! */
-		
+
 		return true; // this line is here only so this code will compile if you don't modify it
 	}
-	
+
 }
