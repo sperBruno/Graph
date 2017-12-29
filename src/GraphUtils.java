256ed0bd48c898ac8c79ae055de7322f9d10f7ff
sperBruno <sperbru797@gmail.com>
2017-12-28 23:02:13 -0400

    update

--- src/GraphUtils.java
+++ src/GraphUtils.java
@@ -15,10 +15,11 @@ public class GraphUtils {
 
 	public static int minDistance(Graph graph, String src, String dest) {
 		int result = -1;
-		if (graph == null || src == null || dest == null || !graph.containsElement(src) || !graph.containsElement(dest)) {
+		if (graph == null || src == null || dest == null || !graph.containsElement(src)
+				|| !graph.containsElement(dest)) {
 			return result;
 		}
-		
+
 		if (Objects.equals(src, dest)) {
 			return 0;
 		}
@@ -28,7 +29,7 @@ public class GraphUtils {
 		if (!nodeExist) {
 			return result;
 		}
-		return bfs.marked.size() > 1 ? bfs.marked.size()/ 2  : 1;
+		return bfs.marked.size() > 1 ? bfs.marked.size() / 2 : 1;
 	}
 
 	public static Set<String> nodesWithinDistance(Graph graph, String src, int distance) {
@@ -67,19 +68,16 @@ public class GraphUtils {
 	}
 
 	public static boolean isHamiltonianPath(Graph graph, List<String> values) {
-        ArrayList<Node> markedNodes = new ArrayList<>();
+		ArrayList<Node> markedNodes = new ArrayList<>();
 		boolean result = false;
-		if (graph != null && values != null && !values.isEmpty()) {
-			if (values.get(0) == values.get(values.size() - 1)) {
+		if (graph != null && values != null && !values.isEmpty() && values.get(0) == values.get(values.size() - 1)) {
 				for (int i = 0; i < values.size() - 1; i++) {
 					if (graph.containsElement(values.get(i))) {
 						Node node = graph.getNode(values.get(i));
 						Node nodeNext = graph.getNode(values.get(i + 1));
 						Set<Edge> adjacencySetsAux = graph.adjacencySets.get(node);
 						if (SetContainsValue(adjacencySetsAux, nodeNext)) {
-					
-							if (markedNodes.contains(node))
-							{
+							if (markedNodes.contains(node)) {
 								return false;
 							}
 							markedNodes.add(node);
@@ -89,17 +87,12 @@ public class GraphUtils {
 					}
 					result = true;
 				}
-			}
-		
+
 			Set<Node> nodesOfGraph = graph.getAllNodes();
 			nodesOfGraph.removeAll(markedNodes);
 			if (!nodesOfGraph.isEmpty())
 				return false;
-			
-			
-
 		}
-
 		return result;
 	}
 
@@ -108,7 +101,6 @@ public class GraphUtils {
 		for (Edge edge : set) {
 			if (edge.getDestination().element.equals(node.element))
 				result = true;
-
 		}
 		return result;
 	}
